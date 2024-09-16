342f11edf81aa2f4aaaf6ba77c585eda354a2f35
spiritual-gif2 <80268472+spiritual-gif2@users.noreply.github.com>
2024-09-16 15:40:14 +0100

    Update database.php - new issues

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-$hostIP = 127.0.0.1;
+$hostIP = '127.0.0.1';
 use Illuminate\Support\Str;
 
 return [
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '$hostIP'),
+            'host' => env('DB_HOST', $hostIP),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
@@ -66,7 +66,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '$hostIP'),
+            'host' => env('DB_HOST', $hostIP),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
@@ -130,7 +130,7 @@ return [
 
         'default' => [
             'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '$hostIP'),
+            'host' => env('REDIS_HOST', $hostIP),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
@@ -139,7 +139,7 @@ return [
 
         'cache' => [
             'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '$hostIP'),
+            'host' => env('REDIS_HOST', $hostIP),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
